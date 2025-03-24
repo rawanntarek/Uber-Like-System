9c2af1c810fe768734851a0173217aad4ffba5da
Rawan Tarek <142927339+rawanntarek@users.noreply.github.com>
2025-03-24 03:50:35 +0200

    fix

--- src/Customer_Features.java
+++ src/Customer_Features.java
@@ -14,13 +14,19 @@ public class Customer_Features {
                 handleRideRequest(message, username, output);
             } else if (message.equals("acceptOffer")) {
                 Accept_Offer(username, output);
-                
+
             } else if (message.equals("declineOffer")) {
                 declineOffer(username, output);
             } else if (message.equals("viewStatus")) {
-                output.writeUTF("View status feature not implemented yet.");
+                viewRideStatus(username, output);
             } else if (message.equals("exit")) {
-                output.writeUTF("exit");
+                if(diconnect(username))
+                {
+                    output.writeUTF("You cannot disconnect");
+                }
+                else {
+                    output.writeUTF("You have been disconnected");
+                }
                 break;
             } else {
                 output.writeUTF("Unknown command.");
@@ -133,7 +139,40 @@ public class Customer_Features {
             output.writeUTF("No more offers available for your ride.");
         }
     }
+    private static void viewRideStatus(String username, DataOutputStream output) throws IOException {
+        Ride latestRide = null;
+
+        for (int i = UberServer.rides.size() - 1; i >= 0; i--) {
+            Ride r = UberServer.rides.get(i);
+            if (r.getCustomerUsername().equals(username)) {
+                latestRide = r;
+                break;
+            }
+        }
+
+        if (latestRide == null) {
+            output.writeUTF("You have not requested any rides yet.");
+            return;
+        }
+
+        String status = latestRide.getStatus();
+        String response = "Your ride (Ride ID: " + latestRide.getRideId() + ") is currently '" + status + "',"+" Assigned driver: " + latestRide.getAssignedDriver();
 
 
+
+        output.writeUTF(response);
+    }
+
+    private static boolean diconnect(String username) {
+        for (Ride r : UberServer.rides) {
+            if (r.getCustomerUsername().equals(username)) {
+                String status = r.getStatus();
+                if (status.equalsIgnoreCase("assigned") || status.equalsIgnoreCase("in progress")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
